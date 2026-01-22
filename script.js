10e096ffbcc34d93319ae5c128379474ee0c4fb9
Klamkh <mohanad.alkafri@gmail.com>
2026-01-22 21:24:07 +0100

    removed reset from numbers and added dic and tried to work on hasError

--- script.js
+++ script.js
@@ -15,6 +15,9 @@ const devi = document.querySelector('.devide')
 const equal = document.querySelector('.equal')
 const show = document.querySelector('.show')
 const erase = document.querySelector('.C')
+const p = document.querySelector('p')
+const btn = document.querySelectorAll('button')
+
 
 const add = function(n1, n2) {return n1+n2 }
 const substract = function(n1, n2) {return n1-n2}
@@ -25,8 +28,11 @@ let n1 = ''
 let n2 = ''
 let op = ''
 let currn = 0
+let hasError = false
 
 const operate = function(n1, n2, op){
+    n1 = Number(n1)
+    n2 = Number(n2)
     if(op == '+') return add(n1,n2)
     else if(op =='-') return substract(n1,n2)
     else if(op=='*') return multiply(n1,n2)
@@ -37,61 +43,60 @@ const reset = function (){
     n1 = Number(n1)
     n2 = Number(n2)
     let currn = operate(n1, n2, op)
-    show.textContent = currn
+    show.textContent = ''
     n1 = currn
-    n2 = ''
+    n2 = ''    
+    show.insertAdjacentText('beforeend', n1)
 }}
+
+const dic = function (n){
+    if(n%1 != 0){
+        n = n.toFixed(2)
+    }
+    return n
+}
+
 
 
 zero.addEventListener('click', e => {
     if(op == '') n1 = n1+'0'
     else n2 = n2 + '0'
-    reset()
-    show.insertAdjacentText('beforeend', '1')})
+    show.insertAdjacentText('beforeend', '0')})
 one.addEventListener('click', e => {
     if(op == '') n1 = n1+'1'
     else n2 = n2 + '1'
-    reset()
     show.insertAdjacentText('beforeend', '1')})
 two.addEventListener('click', e => {
     if(op == '') n1 = n1+'2'
     else n2 = n2 + '2'
-    reset()
     show.insertAdjacentText('beforeend', '2')})
 three.addEventListener('click', e => {
     if(op == '') n1 = n1+'3'
     else n2 = n2 + '3'
-    reset()
     show.insertAdjacentText('beforeend', '3')})
 four.addEventListener('click', e => {
     if(op == '') n1 = n1+'4'
     else n2 = n2 + '4'
-    reset()
     show.insertAdjacentText('beforeend', '4')})
 five.addEventListener('click', e => {
     if(op == '') n1 = n1+'5'
     else n2 = n2 + '5'
-    reset()
     show.insertAdjacentText('beforeend', '5')})
 six.addEventListener('click', e => {
     if(op == '') n1 = n1+'6'
     else n2 = n2 + '6'
-    reset()
     show.insertAdjacentText('beforeend', '6')})
 seven.addEventListener('click', e => {
     if(op == '') n1 = n1+'7'
     else n2 = n2 + '7'
-    reset()
     show.insertAdjacentText('beforeend', '7')})
 eight.addEventListener('click', e => {
     if(op == '') n1 = n1+'8'
     else n2 = n2 + '8'
-    reset()
     show.insertAdjacentText('beforeend', '8')})
 nine.addEventListener('click', e => {
     if(op == '') n1 = n1+'9'
     else n2 = n2 + '9'
-    reset()
     show.insertAdjacentText('beforeend', '9')})
 minus.addEventListener('click', e => {
     reset()
@@ -115,17 +120,21 @@ erase.addEventListener('click', e => {
     show.textContent = ''
     n1 = ''
     n2 = ''
+    p.style.color = 'black'
 })
 equal.addEventListener('click', e => {
-    n1 = Number(n1)
-    n2 = Number(n2)
-    let result = operate(n1, n2, op)
-    show.textContent = result
+    let result = 0
+    if(n2 == 0 && op == '/'){
+        p.style.color = 'red'
+        hasError = true
+        show.textContent = 'ERROR: DEVIDING BY 0.'
+
+    }
+    else if(n2 != '' && n1 != ''){
+        result = operate(n1, n2, op)
+        show.textContent = dic(result)}
+    else if(n2 == ''){
+        n2 = n1
+        result = operate(n1, n2, op)
+        show.textContent = dic(result)}
 })
-//  n1 = '1'
-//  n1 = Number(n1)
-//  n2 = '1'
-//  n2 = Number(n2)
-//  op = '+'
-//  console.log(operate(n1, n2, op))
-//  console.log(typeof n2)
