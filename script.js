e47ba6a54e94a63be3349480ec933a8d52ef356e
Klamkh <mohanad.alkafri@gmail.com>
2026-01-23 01:13:00 +0100

    did what is written on the last commit and added colors to operators

--- script.js
+++ script.js
@@ -1,22 +1,11 @@
-const zero = document.querySelector('.zero')
-const one = document.querySelector('.one')
-const two = document.querySelector('.two')
-const three = document.querySelector('.three')
-const four = document.querySelector('.four')
-const five = document.querySelector('.five')
-const six = document.querySelector('.six')
-const seven = document.querySelector('.seven')
-const eight = document.querySelector('.eight')
-const nine = document.querySelector('.nine')
 const plus = document.querySelector('.plus')
 const minus = document.querySelector('.minus')
 const mult = document.querySelector('.multiply')
 const devi = document.querySelector('.devide')
 const equal = document.querySelector('.equal')
-const show = document.querySelector('.show')
+const display = document.querySelector('.display')
 const erase = document.querySelector('.C')
 const p = document.querySelector('p')
-const btn = document.querySelectorAll('button')
 const btnCon = document.querySelector('.btncontainer')
 
 
@@ -25,11 +14,12 @@ const substract = function(n1, n2) {return n1-n2}
 const multiply = function(n1, n2) {return n1*n2}
 const devide = function(n1, n2) {return n1/n2}
 
+
 let n1 = ''
 let n2 = ''
 let op = ''
-let currn = 0
 let hasError = false
+
 
 const operate = function(n1, n2, op){
     n1 = Number(n1)
@@ -41,15 +31,12 @@ const operate = function(n1, n2, op){
 }
 const reset = function (){
     if(op !='' && n2 != '') {
-    n1 = Number(n1)
-    n2 = Number(n2)
     let currn = operate(n1, n2, op)
-    show.textContent = ''
+    display.textContent = ''
     n1 = currn
     n2 = ''    
-    show.insertAdjacentText('beforeend', n1)
+    display.insertAdjacentText('beforeend', n1)
 }}
-
 const dic = function (n){
     if(n%1 != 0){
         n = n.toFixed(2)
@@ -58,75 +45,45 @@ const dic = function (n){
 }
 
 
-btnCon.addEventListener('click' , e => {
+btnCon.addEventListener('click', e => {
+    if(!e.target.matches('button')) return
+    if(!e.target.classList.contains('number')) return
     if( hasError == true && e.target != equal){
         n1 = ''
         n2 = ''
         op = ''
         p.style.color = 'black'
-        show.textContent = ''
+        display.textContent = ''
         hasError = false
-}})
-zero.addEventListener('click', e => {
-    if(op == '') n1 = n1+'0'
-    else n2 = n2 + '0'
-    show.insertAdjacentText('beforeend', '0')})
-one.addEventListener('click', e => {
-    if(op == '') n1 = n1+'1'
-    else n2 = n2 + '1'
-    show.insertAdjacentText('beforeend', '1')})
-two.addEventListener('click', e => {
-    if(op == '') n1 = n1+'2'
-    else n2 = n2 + '2'
-    show.insertAdjacentText('beforeend', '2')})
-three.addEventListener('click', e => {
-    if(op == '') n1 = n1+'3'
-    else n2 = n2 + '3'
-    show.insertAdjacentText('beforeend', '3')})
-four.addEventListener('click', e => {
-    if(op == '') n1 = n1+'4'
-    else n2 = n2 + '4'
-    show.insertAdjacentText('beforeend', '4')})
-five.addEventListener('click', e => {
-    if(op == '') n1 = n1+'5'
-    else n2 = n2 + '5'
-    show.insertAdjacentText('beforeend', '5')})
-six.addEventListener('click', e => {
-    if(op == '') n1 = n1+'6'
-    else n2 = n2 + '6'
-    show.insertAdjacentText('beforeend', '6')})
-seven.addEventListener('click', e => {
-    if(op == '') n1 = n1+'7'
-    else n2 = n2 + '7'
-    show.insertAdjacentText('beforeend', '7')})
-eight.addEventListener('click', e => {
-    if(op == '') n1 = n1+'8'
-    else n2 = n2 + '8'
-    show.insertAdjacentText('beforeend', '8')})
-nine.addEventListener('click', e => {
-    if(op == '') n1 = n1+'9'
-    else n2 = n2 + '9'
-    show.insertAdjacentText('beforeend', '9')})
+    }
+    let num = e.target.textContent
+    display.insertAdjacentText('beforeend', num)
+    if(op === '') n1 += num
+    else if(op !== '') n2 += num
+})
 minus.addEventListener('click', e => {
     reset()
     op = '-'
-    show.insertAdjacentText('beforeend', '-')})
+    display.insertAdjacentHTML('beforeend',
+         `<span class=operation>${op}</span>`)})
 plus.addEventListener('click', e => {
     reset()
     op = '+'
-    show.insertAdjacentText('beforeend', '+')})
+    display.insertAdjacentHTML('beforeend',
+         `<span class=operation>${op}</span>`)})
 mult.addEventListener('click', e => {
     reset()
     op = '*'
-    show.insertAdjacentText('beforeend', '*')})
+    display.insertAdjacentHTML('beforeend',
+         `<span class=operation>${op}</span>`)})
 devi.addEventListener('click', e => {
     reset()
     op = '/'
-    show.insertAdjacentText('beforeend', '/')})
-
+    display.insertAdjacentHTML('beforeend',
+         `<span class=operation>${op}</span>`)})
 erase.addEventListener('click', e => {
     op = ''
-    show.textContent = ''
+    display.textContent = ''
     n1 = ''
     n2 = ''
     p.style.color = 'black'
@@ -135,15 +92,16 @@ equal.addEventListener('click', e => {
     let result = 0
     if(n2 == 0 && op == '/'){
         hasError = true
-        show.textContent = 'ERROR: DEVIDING BY 0.'
+        display.textContent = 'ERROR: DEVIDING BY 0.'
         p.style.color = 'red'
 
     }
     else if(n2 != '' && n1 != ''){
         result = operate(n1, n2, op)
-        show.textContent = dic(result)}
+        display.textContent = dic(result)}
     else if(n2 == ''){
         n2 = n1
         result = operate(n1, n2, op)
-        show.textContent = dic(result)}
+        display.textContent = dic(result)}
 })
+
